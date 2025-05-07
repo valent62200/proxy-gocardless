e0660af9dde5c4b68757a0a092df255273898aef
Valent62200 <valent.skillz@gmail.com>
2025-05-07 15:19:08 +0200

    ✅ Ajout proxy GoCardless sécurisé avec CORS

--- netlify/functions/gocardless-proxy.js
+++ netlify/functions/gocardless-proxy.js
@@ -1,8 +1,20 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+      },
+      body: ''
+    };
+  }
+
   try {
-    const { path, method, headers, body } = JSON.parse(event.body || '{}');
+    const { path, method, headers = {}, body } = JSON.parse(event.body || '{}');
 
     if (!path || !method) {
       return {
@@ -11,10 +23,19 @@ exports.handler = async (event) => {
       };
     }
 
+    const allowedHeaders = ['Content-Type', 'Authorization'];
+    const filteredHeaders = Object.keys(headers)
+      .filter(k => allowedHeaders.includes(k))
+      .reduce((acc, key) => {
+        acc[key] = headers[key];
+        return acc;
+      }, {});
+
     const apiUrl = `https://bankaccountdata.gocardless.com/api/v2/${path.replace(/^\/+/, '')}`;
+
     const response = await fetch(apiUrl, {
       method,
-      headers,
+      headers: filteredHeaders,
       body: body ? JSON.stringify(body) : undefined,
     });
 
@@ -29,6 +50,7 @@ exports.handler = async (event) => {
       },
       body: JSON.stringify(data),
     };
+
   } catch (error) {
     return {
       statusCode: 500,
